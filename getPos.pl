c28fad1b92d988d03bbeb2c781a6087175e09412
Rob Long <robert.l.long@gmail.com>
2012-08-13 08:13:41 -0500

    add reference

---
+++ getPos.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use IO::File;
+
+my $fh = IO::File->new($ARGV[0]);
+my $ref_path = $ARGV[1];
+
+
+while (my $line = $fh->getline) {
+    chomp $line;
+    if (substr($line, 0, 1) eq '#') {
+        next;
+    }
+    my ($rsid, $chr, $pos, $alleles) = split /\t/, $line;
+    $chr = "chr$chr";
+    my $faidx_string = "samtools faidx $ref_path $chr:$pos-$pos";
+
+    my @result = `$faidx_string`;
+
+    my $ref = $result[1];
+    chomp $ref;
+    print "$chr\t$pos\t$rsid\t$ref\n";
+}
+$fh->close;
---
+++ readme.md
@@ -0,0 +1,10 @@
+hg19 reference bases for 23andme microarray raw data
+=============
+
+In order to create a VCF of one's 23andme raw data, the reference base for each
+position must be known. This file is a tab delimited file of the format:
+
+chromosome	position	rs_id	hg19 reference base
+
+
+
